68eab149b5f6b1373aeb6606ef4bc472631c3206
jitendra_chhipa <jitendrachhipa@gmail.com>
2020-08-20 01:11:56 +0530

    added

---
+++ src/main/java/quick/swap.java
@@ -0,0 +1,18 @@
+package quick;
+
+public class swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x =10;
+		int y =20;
+		int z;
+		z = x;
+		x =y;
+		y = z;
+		System.out.println("print value x"  +"    "+x);
+		System.out.println("print value x"  +"     " +y);
+	}
+
+}
